1c02f7c0a6682464812981133015a1bfc1cb2518
hayaishi <akihiro.hayaishi@gmail.com>
2010-09-12 23:03:53 +0900

    join-string => mapconcat

--- set-perl5lib-path.el
+++ set-perl5lib-path.el
@@ -8,15 +8,6 @@
            t)
           (t nil))))
 
-(defun join-string (separator target-list)
-  (let* (joined-string
-         (target (car target-list)))
-    (if (not (null target-list))
-      (setq joined-string (join-string separator (cdr target-list))))
-    (if (null joined-string)
-        target
-      (concat target separator joined-string))))
-
 (defun set-perl5lib-path (path dir-list)
   (interactive)
   (let* ((target-dir (car dir-list))
@@ -30,7 +21,7 @@
   (let* ((env (getenv "PERL5LIB"))
          (path-list (nreverse (cdr (nreverse (cdr (split-string path "/"))))))
          (counter (length path-list))
-         (path-string (concat "/" (join-string "/" path-list)))
+         (path-string (concat "/" (mapconcat 'identity path-list "/" )))
          (path-work (concat path-string "/" dirname)))
     (if (and
          (stringp path-string)
